454fc98c0a3eb8c7e6260300a96baa09b0b48417
Jacob <jjanow7@uic.edu>
2020-06-12 15:08:47 +0000

    add admin role

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
 			} else {
 				//does user own this campground?
 
-				if(foundCampground.author.id.equals(req.user._id)) {
+				if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
 					next();
 				}
 
@@ -39,7 +39,7 @@ middlewareObj.checkCommentOwnership = function(req,res,next){
 			} else {
 				//does user own this comment?
 
-				if(foundComment.author.id.equals(req.user._id)) {
+				if(foundComment.author.id.equals(req.user._id)|| req.user.isAdmin) {
 					next();
 				}
 
