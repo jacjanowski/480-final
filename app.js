08ce540cf01d9c4119fbb0d49314593e56db3407
jacjanowski <jjanow7@uic.edu>
2021-04-28 17:58:16 -0500

    add comment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require("express"),
 	User = require("./models/user"),
 	seedDB = require("./seeds");
     
-//requiring routes
+//requiring routes comment
 var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes 		 = require("./routes/index");
