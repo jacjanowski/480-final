11253f21dede15c72a22f8a2a95d54e5d4064582
jacjanowski <jjanow7@uic.edu>
2021-05-01 11:42:37 -0500

    add javascript to backend

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,14 +23,15 @@ router.post("/", middleware.isLoggedIn, function(req,res){
 	
 	var name = req.body.name;
 	var image = req.body.image;
-	var price = req.body.price;
+	// var price = req.body.price;
+	var information = req.body.information;
 	var desc = req.body.description;
 	var author = {
 		id: req.user._id,
 		username: req.user.username
 	}
-	var newCampground = {name: name, price: price, image:image, description: desc, author:author};
-
+	var newCampground = {name: name, information:information, image:image, description: desc, author:author};
+	
 	//Create a new campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
 		if(err){
